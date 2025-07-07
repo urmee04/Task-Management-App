6751af0cfa3903867914947e867c4af9d669c5e6
urmee04 <urmee04sust@gmail.com>
2025-07-07 19:08:45 -0400

    Add task data creation logic in processNewTask function

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const newTaskDeadlineField = document.getElementById("deadline");
 const newTaskStatusField = document.getElementById("status");
 const taskContainer = document.getElementById("taskList");
 const filteredTaskContainer = document.getElementById("filteredTaskList");
+
+//Handle new tasks creation
+function processNewTask() {}
+  const taskTitle= newTaskNameField.value.trim();
+  const taskCategory= newTaskCategoryField.ariaValueMax.trim();
+  const taskDueDate = newTaskDeadlineField.
