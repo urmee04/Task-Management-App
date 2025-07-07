647fa37db7ff2867233269fb77a2910cc112afd3
urmee04 <urmee04sust@gmail.com>
2025-07-07 17:47:19 -0400

    define and organize task-related DOM element references

---
+++ index.js
@@ -0,0 +1,7 @@
+//DOM element references
+const newTaskNameField = document.getElementById("taskName");
+const newTaskCategoryField = document.getElementById("category");
+const newTaskDeadlineField = document.getElementById("deadline");
+const newTaskStatusField = document.getElementById("status");
+const taskContainer = document.getElementById("taskList");
+const filteredTaskContainer = document.getElementById("filteredTaskList");
